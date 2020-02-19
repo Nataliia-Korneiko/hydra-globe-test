a9ca7e0d7fd97a4aeb6aab07b81a539d920daa09
Nataliia Korneiko <korneyko_ns@ukr.net>
2020-02-19 22:17:50 +0200

    changed index.js

--- js/index.js
+++ js/index.js
@@ -2,21 +2,15 @@
 const buttonBurger = document.querySelector(".navigation__button-burger");
 const buttonClose = document.querySelector(".navigation__button-close");
 const navigationList = document.querySelector(".navigation__list");
-const linkCoats = document.querySelector(".header__link-coats");
-const linkBrands = document.querySelector(".header__link-brands");
 
 const handleShow = () => {
   navigationList.classList.add("navigation__list-show");
   buttonClose.classList.add("navigation__button-close-show");
-  linkCoats.classList.add("header__link-coat-show");
-  linkBrands.classList.add("header__link-brands-show");
 };
 
 const handleClose = () => {
   navigationList.classList.remove("navigation__list-show");
   buttonClose.classList.remove("navigation__button-close-show");
-  linkCoats.classList.remove("header__link-coat-show");
-  linkBrands.classList.remove("header__link-brands-show");
 };
 
 buttonBurger.addEventListener("click", handleShow);
